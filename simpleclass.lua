bccb6288d53b1229ce172033e4bf5aef073f993c
Wang Hongwei <525269029@qq.com>
2017-08-26 22:47:23 +0800

    Add files via upload

--- simpleclass.lua
+++ simpleclass.lua
@@ -1,17 +1,12 @@
---[[ simpleClass
+--[[ SimpleClass
 	简单的单一继承类,single inheritance
-
-
-
 --]]
 
-local simpleClass = {
+local SimpleClass = {
 	_VERSION = 0.1;
 	_DESCREPTION = [===[
 		Wang Hongwei, email: 525269029@qq.com
-
 	]===];
-
 }
 
 local function isValidClassName(name)
@@ -26,7 +21,7 @@ local function isValidClassName(name)
 end
 
 local function isClass(c)
-	if type(c) == 'table' and c._TYPE =='simpleClass' then
+	if type(c) == 'table' and c._TYPE =='SimpleClass' then
 		return true
 	else
 		return false
@@ -41,14 +36,13 @@ local function creatclass(classname,baseclass)
 	if baseclass and (not isClass(baseclass)) then error(tostring(baseclass)..'is not a valid class') end
 
 	local cls = {}
-	cls._TYPE = 'simpleClass'	-- 使用simpleclass创建的类都添加标识，有这个标识就可以用作基类
-	cls._VERSION = simpleClass._VERSION
+	cls._TYPE = 'SimpleClass'	-- 使用SimpleClass创建的类都添加标识，有这个标识就可以用作基类
+	cls._VERSION = SimpleClass._VERSION
 
 	cls._CLASSNAME = classname	--类名起什么作用呢，怎样区分不同包里相同的类名,尤其在is_a中
 	cls.is_a = {}
 	cls.is_a[cls] = true
 	--cls.is_a[cls._CLASSNAME] = true
-
 
 	local mt = {__call = function(c,...)
 		local ins = {}
@@ -66,8 +60,8 @@ local function creatclass(classname,baseclass)
 		mt.__index = baseclass	--
 		cls.base = baseclass
 		-- 将基类的is_a复制到新类中
-		for v in pairs(baseclass.is_a) do
-			cls.is_a[v] = true
+		for k,v in pairs(baseclass.is_a) do
+			cls.is_a[k] = true
 		end
 	end
 
@@ -76,9 +70,9 @@ local function creatclass(classname,baseclass)
 	return cls
 end
 
-setmetatable(simpleClass,{__call = function(c,...)
+setmetatable(SimpleClass,{__call = function(c,...)
 	return creatclass(...)
 end})
 
 
-return simpleClass
+return SimpleClass
